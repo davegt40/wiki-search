fcdc11326005fdf5dd6f1a84fdc519d4f647c96b
Dave <davegt40@gmail.com>
2017-12-20 23:45:53 -0500

    finished ajax query

--- js/index.js
+++ js/index.js
@@ -6,9 +6,34 @@ function clearInputField(element) {
 	document.getElementById(element).value = null;
 }
 
+function wikiQueryBuilder() {
+	var wikiURL = "https://en.wikipedia.org/w/api.php";
+	wikiURL += '?' + $.param({
+    'action' : 'opensearch',
+    'search' : getSearchString('search1'),
+    'prop'  : 'revisions',
+    'rvprop' : 'content',
+    'format' : 'json',
+    'limit' : 10
+	});
+	return wikiURL;
+}
+
+function search() {
+  var wikiURL = wikiQueryBuilder();
+
+	$.ajax( {
+    url: wikiURL,
+    dataType: 'jsonp',
+    success: function(data) {
+       console.log(data);
+    }
+	});
+}
+
 $(document).ready(function() {
 	$("#search-button").on("click", function() {
-		console.log(getSearchString('search1'));
+		search();
 		clearInputField("search1");
 	});
 });
